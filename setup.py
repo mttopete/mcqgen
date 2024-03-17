9b02c6c5304eceac123269b9ce792ecf1eb12bf7
mttopete <mateotopete@gmail.com>
2024-03-16 17:20:36 -0700

    updated and ran experiments in jupyter notebook

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='mateo topete',
     author_email='mateotopete@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotevn","PyPDF2"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
